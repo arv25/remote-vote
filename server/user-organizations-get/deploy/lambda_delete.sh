dccb7bbda920c37934e630c799f84c73fb158e55
Arvind Budhram <arvind@shopkeep.com>
2016-02-24 22:42:01 -0500

    Adding user-organizations-get lambda module.

---
+++ server/user-organizations-get/deploy/lambda_delete.sh
@@ -0,0 +1,2 @@
+aws lambda delete-function --function-name remote-vote-stag-server-user-organizations-get
+aws lambda delete-function --function-name remote-vote-prod-server-user-organizations-get
---
+++ server/user-organizations-get/index.js
@@ -0,0 +1,14 @@
+var AWS = require('aws-sdk');
+AWS.config.update({region:'us-east-1'});
+var dynamodb = new AWS.DynamoDB({apiVersion: '2012-08-10'});
+
+exports.handler = function(event, context) {
+  console.log("Request received:\n", JSON.stringify(event));
+  console.log("Context received:\n", JSON.stringify(context));    
+
+  var stage_name = event.stage_name;
+  var msg = ["Lambda function invoked successfully on ", stage_name, " environment."].join("");
+
+  console.log(msg);
+  context.succeed(msg);
+}
