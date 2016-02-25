58ee3b48d1ac2f8ba3e7859a0f3f88224eb318ff
Arvind Budhram <arvind@shopkeep.com>
2016-02-25 03:06:04 -0500

    Adding user-organization-member-get lambda module.

---
+++ server/user-organization-member-get/deploy/lambda_delete.sh
@@ -0,0 +1,2 @@
+aws lambda delete-function --function-name remote-vote-stag-server-user-organizations-member-get
+aws lambda delete-function --function-name remote-vote-prod-server-user-organizations-member-get
