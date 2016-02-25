493fd171b07cd48c415c690dd62f9128c6edcf73
Arvind Budhram <arvind@shopkeep.com>
2016-02-25 13:26:43 -0500

    Adding user-organization-election-contender-vote-post lambda module.

---
+++ server/user-organization-election-contender-vote-post/deploy/lambda_delete.sh
@@ -0,0 +1,2 @@
+aws lambda delete-function --function-name remote-vote-stag-server-user-org-election-contender-vote-post
+aws lambda delete-function --function-name remote-vote-prod-server-user-org-election-contender-vote-post
