bd2a4b0762e403fcda7f5f77b4c8debea00fb47c
Arvind Budhram <arvind@shopkeep.com>
2016-02-27 04:57:34 -0500

    Updating vote count to decrement old vote and increment new vote when appropriate.

--- server/vote-aggregator/index.js
+++ server/vote-aggregator/index.js
@@ -2,18 +2,21 @@ var AWS = require('aws-sdk');
 AWS.config.update({region:'us-east-1'});
 var dynamodb = new AWS.DynamoDB({apiVersion: '2012-08-10'});
 
+var deepEqual = require('deep-equal');
 var uuid = require('uuid');
 var Promise = require("bluebird");
 Promise.promisifyAll(Object.getPrototypeOf(dynamodb));
 
 function generateResultIdCompositeKey(event) {
-  return [event.organization_id, event.election_id, event.contender_id, event.vote_enum].join("--");
+  return [event.vote_enum].join(""); //, event.election_id, event.contender_id, event.vote_enum].join("--");
 };
 
 function incrementCounter(event, context, valueToIncrement) {
   var stage_name = event.stage_name;
   var results_table_name = ["remote-vote-", stage_name, "-results"].join("");
   var generated_result_id = generateResultIdCompositeKey(event);
+
+  console.log("got to putItem code");
 
   dynamodb.putItemAsync({
     "TableName": results_table_name,
@@ -34,6 +37,8 @@ function incrementCounter(event, context, valueToIncrement) {
     }
   }).finally(function(data) {
 
+    console.log("got to updateItem code");
+
     dynamodb.updateItemAsync({
       "TableName": results_table_name,
       "Key": {
@@ -41,7 +46,7 @@ function incrementCounter(event, context, valueToIncrement) {
           "S": generated_result_id
         }
       },
-      "UpdateExpression":  "add num_votes :num",
+      "UpdateExpression": "add num_votes :num",
       "ExpressionAttributeValues": {
         ":num" : { "N": valueToIncrement.toString() }
       }
@@ -51,34 +56,66 @@ function incrementCounter(event, context, valueToIncrement) {
       context.succeed(success_msg);
 
     }).catch(function(err) {
-      var failure_msg = ['ERROR: Remote-Vote-', stage_name, '-results failed to complete. - ', err].join("");
+      var failure_msg = ['ERROR: Remote-Vote-', stage_name, '-results failed to completion UPDATE operation. - ', generated_result_id, '--', err].join("");
       console.log(failure_msg);
       context.fail(failure_msg);
 
     });
   }).catch(function(ConditionalCheckFailedException, err) {
+     console.log("got to row exists fail case");
 
-  }).catch(function(err) {
-    var failure_msg = ['ERROR: Remote-Vote-', stage_name, '-results failed to complete. - ', err].join("");
-    console.log(failure_msg);
-    context.fail(failure_msg);
+   }).catch(function(err) {
+     console.log("got to row exists fail case");
 
-  });
+     var failure_msg = ['ERROR: Remote-Vote-', stage_name, '-results failed to complete on PUT operation. - ', err].join("");
+     console.log(failure_msg);
+     context.fail(failure_msg);
+
+   });
 };
+
+
+function oldVoteExists(event) {
+  return Object.keys(event["existing_vote"] || {}).length > 0
+};
+
+
+function votesDiffer(event) {
+  var new_vote = {
+    "user_id" : event.user_id,
+    "organization_id" : event.organization_id,
+    "election_id" : event.election_id,
+    "contender_id" : event.contender_id,
+    "vote_enum" : event.vote_enum
+  };
+
+  var results = !deepEqual(event["existing_vote"], new_vote);
+  console.log(event["existing_vote"]);
+  console.log(new_vote);
+  console.log("diff? == " + results);
+
+  return results;
+};
+
+
+function old_vote_with_env(event) {
+  var oldie = event["existing_vote"];
+  oldie["stage_name"] = event.stage_name;
+  console.log(oldie);
+  return oldie;
+};
+
 
 exports.handler = function(event, context) {
   console.log("Request received:\n", JSON.stringify(event));
-  console.log("Context received:\n", JSON.stringify(context));    
+  console.log("Context received:\n", JSON.stringify(context));
 
-  var stage_name = event.stage_name;
-  var table_name = ["remote-vote-", stage_name, "-votes"].join("");
-
-  console.log("Old event vote id: " + JSON.stringify(event.existing_vote));
-
-  // When there's no old vote, just go ahead and aggregate the appropriate counter.
-
-  incrementCounter(event, context, 1);
-
-
-  // context.succeed("foo goodness:  " + JSON.stringify(event));
+  if( oldVoteExists(event) ) {
+    if( votesDiffer(event) ) {
+      incrementCounter(old_vote_with_env(event), context, -1);
+      incrementCounter(event, context, 1);
+    }
+  } else {
+    incrementCounter(event, context, 1);
+  }
 };
