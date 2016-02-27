6f0a57fa7973d7980c5242a6576f3187c1a63140
Arvind Budhram <arvind@shopkeep.com>
2016-02-27 04:59:45 -0500

    Updating vote post to chain call to vote-aggregator lambda passing it both new and old votes.

--- server/user-organization-election-contender-vote-post/index.js
+++ server/user-organization-election-contender-vote-post/index.js
@@ -2,6 +2,7 @@ var AWS = require('aws-sdk');
 AWS.config.update({region:'us-east-1'});
 var dynamodb = new AWS.DynamoDB({apiVersion: '2012-08-10'});
 
+var extend = require('extend');
 var Promise = require("bluebird");
 Promise.promisifyAll(Object.getPrototypeOf(dynamodb));
 
@@ -75,9 +76,45 @@ function electionOpenForVoting(event, context) {
 };
 
 
+function dynamoStringDataToHash(data) {
+  var items = data["Items"];
+  var results = {"existing_vote": {} };
+
+  items.map(function(row) {
+    for(var key in row) {
+      results["existing_vote"][key] = row[key]["S"];
+    }
+  });
+
+  return results;
+};
+
+
 function generateVoteIdCompositeKey(event) {
   return event.user_id + event.organization_id + event.election_id + event.contender_id
 }
+
+
+function existingVote(event, context){
+  var stage_name = event.stage_name;
+  var votes_table_name = ["remote-vote-", stage_name, "-votes"].join("");
+  var generated_vote_id = generateVoteIdCompositeKey(event);
+
+  return dynamodb.queryAsync({
+    "TableName": votes_table_name,
+    "KeyConditionExpression": "vote_id = :v1",
+    "ExpressionAttributeValues": {
+      ":v1": { "S": generated_vote_id }
+    }
+  }).then(function(data) {
+    return dynamoStringDataToHash(data);
+
+  }).catch(function(err) {
+    console.log(err);
+    context.fail(err);
+  });
+};
+
 
 function persist_vote(event, context) {
   var stage_name = event.stage_name;
@@ -133,12 +170,28 @@ exports.handler = function(event, context) {
       referentialIntegrity(context, "remote-vote-"+stage_name+"-elections", "election_id", event.election_id),
       referentialIntegrity(context, "remote-vote-"+stage_name+"-contenders", "contender_id", event.contender_id),
       electionOpenForVoting(event, context),
-      function(users, orgs, elections, contenders, openElections) {
+      existingVote(event, context),
+      function(users, orgs, elections, contenders, openElections, existingVote) {
         var shouldBePersisted = users && orgs && elections && contenders && openElections;
         console.log("Should be persisted: " + shouldBePersisted);
 
         if (shouldBePersisted) {
-          persist_vote(event, context);
+          var payload = extend(true, existingVote, event);
+
+          var lambda = new AWS.Lambda();
+          lambda.invoke({
+            FunctionName: "remote-vote-" + stage_name + "-server-vote-aggregator",
+            Payload: JSON.stringify(payload)
+
+          }, function(err, data) {
+            if(err) {
+              console.log(err);
+
+            } else {
+              //console.log("Data from lambda cascade: " + JSON.stringify(data));
+              persist_vote(event, context);
+            }
+          });
         };
       });
 };
